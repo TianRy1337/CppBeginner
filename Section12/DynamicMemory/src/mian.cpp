4e085b92f8f3e5a2852f9cab859c219c6314ad94
TianRy1337 <Ryan55014TianZhang@gmail.com>
2022-04-28 09:54:55 +0800

    Section 12
    Dereference, Dynamic Memory

---
+++ Section12/DynamicMemory/src/mian.cpp
@@ -0,0 +1,26 @@
+// Section 12 Dynamic Memory
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    
+    int *ptr {nullptr};
+    ptr = new int;          // allocate the int on the heap
+    cout << ptr << endl;    // use it
+    delete ptr;             // release it
+    
+    size_t size{0};
+    double *dptr {nullptr};
+    
+    cout << "How many temps? ";
+    cin >> size;
+    
+    dptr = new double[size];    // allocate the storage on the heap
+    cout << dptr << endl;       // use it
+    delete [] dptr;                     // release it
+    
+    return 0;
+}
+

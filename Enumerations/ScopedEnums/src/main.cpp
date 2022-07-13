6cc6610eaae6d9005e290d90a89b5f89c4c1adc0
TianRy1337 <Ryan55014TianZhang@gmail.com>
2022-07-13 14:04:29 +0800

    Enums Done

--- Enumerations/ScopedEnums/src/main.cpp
+++ Enumerations/ScopedEnums/src/main.cpp
@@ -26,6 +26,7 @@ std::ostream &operator<<(std::ostream &os, Grocery_Item grocery_Item)
         os << "Invalid item";
         break;
     }
+    return os;
 }
 
 // Used for test1
@@ -88,9 +89,117 @@ void test1()
     display_grocery_list(shopping_list);
 }
 
+// Used for test2
+// A simple class that might model a Player in a game application
+// The player has a name, a mode that they are currently in, and a 
+// direction which they are facing.
+class Player {
+    friend std::ostream &operator<< (std::ostream &os, const Player &p);
+public:
+    enum class Mode {Attack, Defense, Idle};
+    enum class Direction {North, South, East, West};
+
+    Player(std::string name, Mode mode = Mode::Idle, Direction direction = Direction::North):
+        name{name}, mode{mode}, direction{direction} {}
+
+    std::string get_name() const{
+        return name;
+    }
+    void set_name(std::string name){
+        this->name = name;
+    }
+    Mode get_mode() const{
+        return mode;
+    }
+    void set_mode(Mode mode){
+        this->mode = mode;
+    }
+    Direction get_direction() const{
+        return direction;
+    }
+    void set_direction(Direction direction){
+        this->direction = direction;
+    }
+private:
+    std::string name;
+    Mode mode;
+    Direction direction;
+};
+
+// Used for test2
+// A simple function that returns the string representation
+// of the Player::Mode paramter passed into it.
+std::string get_player_mode(Player::Mode mode){
+    std::string result;
+    switch (mode){
+    case Player::Mode::Attack:
+        result = "Attack";
+        break;
+    case Player::Mode::Defense:
+        result = "Defense";
+        break;
+    case Player::Mode::Idle:
+        result = "Idle";
+        break;
+    default:
+        result = "Unknown Mode";
+        break;
+    }
+    return result;
+}
+
+// Used for test2
+// A simple function that returns the string representation
+// of the Player::Direction paramter passed into it.
+std::string get_player_direction(Player::Direction direction){
+    std::string result;
+    switch (direction)
+    {
+    case Player::Direction::North:
+        result = "North";
+        break;
+    case Player::Direction::South:
+        result = "South";
+        break;
+    case Player::Direction::West:
+        result = "West";
+        break;
+    case Player::Direction::East:
+        result = "East";
+        break;
+    default:
+        result = "Unknown Direction!";
+        break;
+    }
+    return result;
+}
+
+// Used for test2
+// Overloading the iutput stream insertion operator
+// so we can easily put Player objects on the output stream.
+std::ostream &operator<<(std::ostream &os, const Player &p){
+    os << "Player name: " << p.get_name() << "\n"
+       << "Player mode: " << get_player_mode(p.mode) << "\n"
+       << "Player direction: " << get_player_direction(p.get_direction())
+       << std::endl;
+    return os;
+}
+
+void test2(){
+    std::cout << "\n--- Test2 ------------------------------------\n" <<std::endl;
+    Player p1{"Cloud Strife", Player::Mode::Attack, Player::Direction::North};
+    Player p2{"Tifa Lockhart", Player::Mode::Defense, Player::Direction::West};
+    Player p3{"Sephiroth", Player::Mode::Idle, Player::Direction::South};
+
+    std::cout << p1 << std::endl;
+    std::cout << p2 << std::endl;
+    std::cout << p3 << std::endl;
+}
+
 int main()
 {
-    test1();
+    // test1();
+    test2();
     std::cout << "\n";
     return 0;
 }

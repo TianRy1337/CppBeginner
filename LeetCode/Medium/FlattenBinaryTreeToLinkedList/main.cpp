11ddb6a8e0ffec0dd0deb378eaeb4755ba2fed99
TianRy1337 <Ryan55014TianZhang@gmail.com>
2022-07-27 23:56:47 +0800

    LeetCode Practice
    114. Flatten Binary Tree to Linked List

--- LeetCode/Medium/FlattenBinaryTreeToLinkedList/main.cpp
+++ LeetCode/Medium/FlattenBinaryTreeToLinkedList/main.cpp
@@ -11,8 +11,28 @@ struct TreeNode {
 
 class Solution {
 public:
+    
+    TreeNode* rightmost(TreeNode* root){
+        if (root->right==NULL) return root;
+        return rightmost(root->right);
+    }
+    
     void flatten(TreeNode* root) {
-
+        if (root==NULL) return;
+        TreeNode* nextright;
+        TreeNode* rightMOST;
+        
+        while (root){
+            
+            if (root->left){
+                rightMOST = rightmost(root->left);
+                nextright = root->right;
+                root->right = root->left;
+                root->left=NULL;
+                rightMOST->right=nextright;
+            }
+            root=root->right;
+        }
     }
 };
 
@@ -20,12 +40,21 @@ void printNode(TreeNode* root){
     if(root->val != NULL){
         cout << root->val << " ";
     }
-    if(root->left != NULL){
+    if(root->left != NULL &&root->right != NULL){
         printNode(root->left);
-    }
-    if(root->right != NULL){
         printNode(root->right);
     }
+    if(root->left == NULL &&root->right != NULL){
+        cout << "null ";
+        printNode(root->right);
+    }
+    else if(root->left != NULL &&root->right == NULL){
+        printNode(root->left);
+    }
+    
+    
+    
+    
 }
 
 int main (){
@@ -42,14 +71,32 @@ int main (){
     Root.left->left = &left2;
     Root.left->right = &right2;
 
-    TreeNode left3 = TreeNode(6);
-    TreeNode right3 = TreeNode(7);
+    // TreeNode right3 = TreeNode(6);
+    
+    // Root.right->right = &right3;
 
-    Root.right->left = &left3;
-    Root.right->right = &right3;
     cout << "[ " ;
     printNode(&Root);
     cout << "]" << endl;
+    
 
-    return 0;
+    Solve.flatten(&Root);
+
+    cout << "[ " ;
+    printNode(&Root);
+    cout << "]" << endl;
+    // TreeNode Root = TreeNode(1);
+    // TreeNode Right = TreeNode(2);
+    // TreeNode Right1 = TreeNode(3);
+    // TreeNode Right2 = TreeNode(4);
+    // TreeNode Right3 = TreeNode(5);
+    // TreeNode Right4 = TreeNode(6);
+    // Root.right = &Right;
+    // Right.right = &Right1;
+    // Right1.right = &Right2;
+    // Right2.right = &Right3;
+    // Right3.right = &Right4;
+    // printNode(&Root);
+
+     return 0;
 }
